6a1919e91de408839f7e62521821abc829eeecbe
Sandip Kharel <135078781+KharelSandip@users.noreply.github.com>
2025-12-28 21:15:03 -0800

    Create EKS module in dev environment
    Add EKS module configuration for development environment

---
+++ terraform/envs/dev/main.tf
@@ -0,0 +1,9 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  # We'll add VPC + subnets next (or use module vpc)
+}
